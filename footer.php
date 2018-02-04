84b243737a1f59590c50c483e1af5eb38527d100
hamza202 <k8668975@gmail.com>
2018-02-04 14:58:20 +0200

    update 2

---
+++ footer.php
@@ -0,0 +1,251 @@
+<!-- footer -->
+<div class='footer_wrapper_copyright'>
+    <!-- canvas -->
+    <div class='half_sin_wrapper'>
+        <canvas class='half_sin' data-bg-color='23,108,129' data-line-color='23,108,129'></canvas>
+    </div>
+    <!-- / canvas -->
+    <footer class='page_footer'>
+        <div class='container'>
+            <div class='footer_container'>
+                <!-- widget post -->
+                <div class="col-md-3 col-sm-6 col-xs-12 cws-widget">
+                    <div class="widget-title">Our News</div>
+                    <div class='widget_carousel'>
+                        <div class='item'>
+                            <!-- post item -->
+                            <div class='post_item'>
+                                <div class='post_preview clearfix'>
+                                    <a href='blog-post.html' class='post_thumb_wrapp pic'><img class='post_thumb' src='pic/bfi_thumb/58x58-portfolio_7-304vqm0b4ukllinncov4ei.jpg' data-at2x='pic/bfi_thumb/58x58-portfolio_7-304vqm0b4ukllinncov4ei@2x.jpg' alt />
+                                        <div class='hover-effect'></div>
+                                        <div class='links'><span class='fa fa-link'></span></div>
+                                    </a>
+                                    <div class='post_title'><a href='blog-post.html'>Morbi iaculis</a></div>
+                                    <div class='post_content'>Quisque in nulla ut metus <a href='blog-post.html'>...</a></div>
+                                    <div class='post_date'>March 24, 2015</div>
+                                </div>
+                            </div>
+                            <!-- / post item -->
+                            <!-- post item -->
+                            <div class='post_item'>
+                                <div class='post_preview clearfix'>
+                                    <a href='blog-post.html' class='post_thumb_wrapp pic'><img class='post_thumb' src='pic/bfi_thumb/58x58-blog_3_col_3-304vqj49s4b9oehlhrm1hm.jpg' data-at2x='pic/bfi_thumb/58x58-blog_3_col_3-304vqj49s4b9oehlhrm1hm@2x.jpg' alt />
+                                        <div class='hover-effect'></div>
+                                        <div class='links'><span class='fa fa-link'></span></div>
+                                    </a>
+                                    <div class='post_title'><a href='blog-post.html'>Praesent sagittis</a></div>
+                                    <div class='post_content'>Maecenas vel elit at dui <a href='blog-post.html'>...</a></div>
+                                    <div class='post_date'>March 24, 2015</div>
+                                </div>
+                            </div>
+                            <!-- / post item -->
+                            <!-- post item -->
+                            <div class='post_item'>
+                                <div class='post_preview clearfix'>
+                                    <a href='blog-post.html' class='post_thumb_wrapp pic'><img class='post_thumb' src='pic/bfi_thumb/58x58-portfolio_4-304vqkvlk7rwl2xsvw77yi.jpg' data-at2x='pic/bfi_thumb/58x58-portfolio_4-304vqkvlk7rwl2xsvw77yi@2x.jpg' alt />
+                                        <div class='hover-effect'></div>
+                                        <div class='links'><span class='fa fa-link'></span></div>
+                                    </a>
+                                    <div class='post_title'><a href='blog-post.html'>Morbi iaculis euismod</a></div>
+                                    <div class='post_content'>Aenean molestie faucibus <a href='blog-post.html'>...</a></div>
+                                    <div class='post_date'>March 22, 2015</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='item'>
+                            <!-- post item -->
+                            <div class='post_item'>
+                                <div class='post_preview clearfix'>
+                                    <div class='post_title'><a href='blog-post.html'>Gallery post</a></div>
+                                    <div class='post_content'>Quisque in nulla ut metus <a href='blog-post.html'>...</a></div>
+                                    <div class='post_date'>March 20, 2015</div>
+                                </div>
+                            </div>
+                            <!-- / post item -->
+                            <!-- post item -->
+                            <div class='post_item'>
+                                <div class='post_preview clearfix'>
+                                    <a href='blog-post.html' class='post_thumb_wrapp pic'><img class='post_thumb' src='pic/bfi_thumb/58x58-blog_3_col_2-304vqh8f5qbfzoaipru7ey.jpg' data-at2x='pic/bfi_thumb/58x58-blog_3_col_2-304vqh8f5qbfzoaipru7ey@2x.jpg' alt />
+                                        <div class='hover-effect'></div>
+                                        <div class='links'><span class='fa fa-link'></span></div>
+                                    </a>
+                                    <div class='post_title'><a href='blog-post.html'>Ut wisi enim</a></div>
+                                    <div class='post_content'>Aenean molestie faucibus <a href='blog-post.html'>...</a></div>
+                                    <div class='post_date'>February 28, 2015</div>
+                                </div>
+                            </div>
+                            <!-- / post item -->
+                            <!-- post item -->
+                            <div class='post_item'>
+                                <div class='post_preview clearfix'>
+                                    <a href='blog-post.html' class='post_thumb_wrapp pic'><img class='post_thumb' src='pic/bfi_thumb/58x58-blog_3_col_1-304vqguumupvnj1wk6a8lm.jpg' data-at2x='pic/bfi_thumb/58x58-blog_3_col_1-304vqguumupvnj1wk6a8lm@2x.jpg' alt />
+                                        <div class='hover-effect'></div>
+                                        <div class='links'><span class='fa fa-link'></span></div>
+                                    </a>
+                                    <div class='post_title'><a href='blog-post.html'>Image post</a></div>
+                                    <div class='post_content'>Quisque in nulla ut metus <a href='blog-post.html'>...</a></div>
+                                    <div class='post_date'>February 25, 2015</div>
+                                </div>
+                            </div>
+                            <!-- / post item -->
+                        </div>
+                    </div>
+                </div>
+                <!-- / widget post -->
+                <!-- twitter -->
+                <div class="col-md-3 col-sm-6 col-xs-12  cws-widget">
+                    <div class="widget-title">Fast Link</div>
+                    <ul class="list-unstyled text-capitalize fast-link">
+                        <li><a href="#">About Us </a> <i class="fa fa-angle-right"></i></li>
+                        <li><a href="#">Features </a> <i class="fa fa-angle-right"></i></li>
+                        <li><a href="#">Services </a> <i class="fa fa-angle-right"></i></li>
+                        <li><a href="#">Our Team </a> <i class="fa fa-angle-right"></i></li>
+                        <li><a href="#">Blog </a> <i class="fa fa-angle-right"></i></li>
+                    </ul>
+                </div>
+                <!-- / twitter -->
+                <!-- widget gallery -->
+                <div class="col-md-3 col-sm-6 col-xs-12 cws-widget">
+                    <div class="widget-title">Gallery</div>
+                    <div class='cws_textwidget_content'>
+                        <div class='text'>
+                            <div id='gallery-2' class='gallery galleryid-2288 gallery-columns-3 gallery-size-thumbnail'>
+                                <!-- gallery item -->
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/02/blog_3_col_2-150x150.jpg" class="attachment-thumbnail" alt="blog_3_col_2" />
+                                    </div>
+                                </div>
+                                <!-- / galery item -->
+                                <!-- galery item -->
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/02/blog_3_col_1-150x150.jpg" class="attachment-thumbnail" alt="blog_3_col_1" />
+                                    </div>
+                                </div>
+                                <!-- / galery item -->
+                                <!-- galery item -->
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/04/pic_double_sidebar_1-150x150.jpg" class="attachment-thumbnail" alt="pic_double_sidebar_1" />
+                                    </div>
+                                </div>
+                                <br style="clear: both" />
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/04/pic_typorgaphy-150x150.jpg" class="attachment-thumbnail" alt="pic_typorgaphy" />
+                                    </div>
+                                </div>
+                                <!-- / galery item -->
+                                <!-- galery item -->
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/08/portfolio_3-150x150.jpg" class="attachment-thumbnail" alt="portfolio_3" />
+                                    </div>
+                                </div>
+                                <!-- / galery item -->
+                                <!-- galery item -->
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/08/portfolio_6-150x150.jpg" class="attachment-thumbnail" alt="portfolio_6" />
+                                    </div>
+                                </div>
+                                <br style="clear: both" />
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/08/portfolio_4-150x150.jpg" class="attachment-thumbnail" alt="portfolio_4" />
+                                    </div>
+                                </div>
+                                <!-- / galery item -->
+                                <!-- galery item -->
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon landscape'>
+                                        <img width="150" height="150" src="pic/2015/08/portfolio_7-150x150.jpg" class="attachment-thumbnail" alt="portfolio_7" />
+                                    </div>
+                                </div>
+                                <!-- / galery item -->
+                                <!-- galery item -->
+                                <div class='gallery-item'>
+                                    <div class='gallery-icon'>
+                                        <img width="1" height="1" src="pic/2015/03/blog_3_col_3.jpg" class="attachment-thumbnail" alt="blog_3_col_3" />
+                                    </div>
+                                </div>
+                                <!-- / gallery item -->
+                                <br style="clear: both" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- / widget gallery -->
+                <!-- conatact form -->
+                <div class="col-md-3 col-sm-6 col-xs-12 cws-widget">
+                    <div class="widget-title">Shoot a Message</div>
+                    <div class="textwidget">
+                        <div role="form" class="cf" id="cf-f1705-o1" lang="en-US" dir="ltr">
+                            <!-- <div class="screen-reader-response"></div> -->
+                            <form action="http://html.creaws.com/kiddy/php/contacts-process.php" method="post" class="cf-form contact-form" novalidate="novalidate">
+                                <p><span class="cf-form-control-wrap your-name"><input type="text" name="name" value="" size="60" class="" aria-required="true" aria-invalid="false" placeholder="Name" /></span></p>
+                                <p><span class="cf-form-control-wrap your-email"><input type="email" name="email" value="" size="60" class="" aria-required="true" aria-invalid="false" placeholder="E-mail" /></span></p>
+                                <p><span class="cf-form-control-wrap your-message"><textarea name="message" cols="39" rows="6" class="" aria-invalid="false" placeholder="Message"></textarea></span></p>
+                                <p>
+                                    <input type="submit" value="Send" class="cf-form-control cf-submit" />
+                                </p>
+                            </form>
+                            <div class="email_server_responce"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- / contact form -->
+            </div>
+        </div>
+    </footer>
+    <!-- copyright -->
+    <div class='copyrights_area'>
+        <!-- canvas -->
+
+        <!-- / canvas -->
+        <div class='container'>
+            <div class='copyrights_container'>
+                <div class='copyrights'>Copyrights ©2016: Kiddy - Premium Children HTML Template</div>
+                <div class='copyrights_panel'>
+                    <div class='copyrights_panel_wrapper'>
+                        <div class="lang_bar">
+                            <div>
+                                <ul>
+                                    <li>
+                                        <a href="#" class="lang_sel_sel icl-en"><img class="iclflag" src="img/en.png" alt="en" title="English" /> &nbsp;English
+                                        </a>
+                                        <ul>
+                                            <li class="icl-fr">
+                                                <a href="#"><img class="iclflag" src="img/1.jpg" alt="ar" title="العربية" />&nbsp;العربية</a>
+                                            </li>
+
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class='cws_social_links'><a href='http://twitter.com/' class='cws_social_link' title='Twitter'><i class='share-icon fa fa-twitter'></i></a><a href='http://facebook.com/' class='cws_social_link' title='Facebook'><i class='share-icon fa fa-facebook'></i></a><a href='http://dribbble.com/' class='cws_social_link' title='Dribbble'><i class='share-icon fa fa-dribbble'></i></a><a href='https://plus.google.com/' class='cws_social_link' title='Google'><i class='share-icon fa fa-google-plus'></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / copyright -->
+</div>
+</div>
+<!-- #page -->
+<div class='scroll_top animated'></div>
+<div id="lang_sel_footer">
+    <ul>
+        <li>
+            <a href="index.html" class="lang_sel_sel"><img src="img/en.png" alt="English" class="iclflag" title="English" />&nbsp;English</a>
+        </li>
+        <li>
+            <a href="#"><img src="img/fr.png" alt="Français" class="iclflag" title="Français" />&nbsp;Français</a>
+        </li>
+        <li>
+            <a href="#"><img src="img/de.png" alt="Deutsch" class="iclflag" title="Deutsch" />&nbsp;Deutsch</a>
+        </li>
+    </ul>
+</div>
---
+++ top-panel.php
@@ -0,0 +1,33 @@
+<div class='site_top_panel wave slider'>
+    <!-- canvas -->
+    <div class='top_half_sin_wrapper'>
+        <canvas class='top_half_sin' data-bg-color='#ffffff' data-line-color='#ffffff'></canvas>
+    </div>
+    <!-- / canvas -->
+    <div class='container'>
+        <div class='row_text_search'>
+            <div id='top_panel_text'><a href="tel:1-800-123-45678"><i class="fa fa-phone-square"></i> 1-800-123-45678 </a>;
+                <a href="mailto:info@info.com"> <i class="fa fa-envelope-o"></i><span class="__cf_email__">info@info.com</span></a>
+            </div>
+        </div>
+        <div id='top_panel_links'>
+            <div class="lang_bar">
+                <div>
+                    <ul>
+                        <li>
+                            <a href="#" class="lang_sel_sel icl-en"><img class="iclflag" src="img/en.png" alt="en" title="English" /> &nbsp;English
+                            </a>
+                            <ul>
+                                <li class="icl-fr">
+                                    <a href="#"><img class="iclflag" src="img/1.jpg" alt="ar" title="العربية" />&nbsp;العربية</a>
+                                </li>
+
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="site_top_panel_toggle"></div>
+    </div>
+</div>
